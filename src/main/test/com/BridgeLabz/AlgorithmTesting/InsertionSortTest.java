96d9c125d98ad159dd7ae29be3edfb679fe03773
Vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-01-02 18:36:54 +0530

    [vikash] isertion sort testing

--- src/main/test/com/BridgeLabz/AlgorithmTesting/InsertionSortTest.java
+++ src/main/test/com/BridgeLabz/AlgorithmTesting/InsertionSortTest.java
@@ -5,29 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 
 import com.bridgeLabz.util.Utility;
-
+/*
+ *  implementation for Insertion sort
+ */
 class InsertionSortTest {
-
 	@Test
 	void test() {
-		try {
-			System.out.println("SORTING USING THE INSERTION SORTING: ");
-			System.out.println("ENTER HOW MANY NUMBER DO YOU WANT TO ENTER IN ARRAY: ");
-			int num = Utility.inputNumber();
-			System.out.println("ENTER YOUR ELEMENTS IN ARRAY: ");
-			int[] actual = Utility.inserting(num);
-			System.out.println("Enter your Expected array: ");
-			int [] expected = Utility.inserting(num);
-			Utility.scanner.close();
-			int[] sortedArray = Utility.isertionSort(actual);
-			System.out.println("YOUR SORTED ARRAY ARE: ");
-			assertArrayEquals(expected, actual);
-			for (int i : sortedArray) {
-				System.out.print(i + " ");
-			}
-		} catch (Exception e) {
-			System.err.println("ENTER VALID INPUT PLEASE: ");
-		}
+
+		int[] expected = { 1, 2, 3, 4 };
+		int[] array = { 2, 4, 1, 3 };
+		int[] actual = Utility.isertionSort(array);
+		assertArrayEquals(expected, actual);
+
+	}
+	
+	
+	@Test
+	void test2() {
+		int[] expected = { 2, 3, 4, 5};
+		int[] array = { 2, 4, 3, 5};
+		int[] actual = Utility.isertionSort(array);
+		assertArrayEquals(expected, actual);
+
+
 	}
 
 }
