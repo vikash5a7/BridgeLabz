f2af5ca82b137aa7bede6f875dc266624575f541
Vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-01-02 11:48:44 +0530

    [vikash] harmonic number Testing

--- src/main/java/com/BridgeLabz/LogicalProgram/Harmoninumber.java
+++ src/main/java/com/BridgeLabz/LogicalProgram/Harmoninumber.java
@@ -7,8 +7,10 @@ public class Harmoninumber
 	public static void main(String[] args) {
 		System.out.println("Please enter the Nth harmonic value:");
 		int number = utility.inputNumber();
+		harmonicNumber(number);
+	}
 
-		
+	public static double harmonicNumber(int number) {
 		final float a = 1;
 		double harmonic = 0;
 		if (number > 0) {
@@ -21,5 +23,7 @@ public class Harmoninumber
 		} else {
 			System.out.println("Enter a number which is greater  0 : ");
 		}
+		return harmonic;
+		
 	}
 }
