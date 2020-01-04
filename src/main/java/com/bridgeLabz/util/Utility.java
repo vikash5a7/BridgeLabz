9e681b6b269d63f4c2579974965ba8ff5d6bb489
Vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-01-04 17:56:30 +0530

    [vikash] Refactor Utility adding logic of anagram

--- src/main/java/com/bridgeLabz/util/Utility.java
+++ src/main/java/com/bridgeLabz/util/Utility.java
@@ -564,11 +564,10 @@ public class Utility {
 	 */
 	 public static boolean checkAnagram(String s1, String s2)
 	    {
-	        // Remove all the white space
 	        s1 = s1.replaceAll("\\s", "");
 	        s2 = s2.replaceAll("\\s", "");
 	        
-	        // Check if both length matches
+	        
 	        if(s1.length() != s2.length())
 	            return false;
 	        else
