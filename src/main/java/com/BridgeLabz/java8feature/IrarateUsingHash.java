0ef38f1756e9462d81e40b161b5449e4fa6d9c63
Vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-01-06 12:46:34 +0530

    [vikash]Add. java8 featur for each fo hash map

---
+++ src/main/java/com/BridgeLabz/java8feature/IrarateUsingHash.java
@@ -0,0 +1,28 @@
+package com.BridgeLabz.java8feature;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+
+public class IrarateUsingHash {
+	public static void main(String[] args) {
+		Map<Integer, Employe> map = new HashMap<>();
+		map.put(1998, new Employe("vikash", "vikas5a@gmail.com", 122.0));
+		map.put(1918, new Employe("raj", "rajk@gmail.com", 122.0));
+		map.put(19918, new Employe("sunyy", "suuny@gmail.com", 10098.0));
+		map.put(9928, new Employe("vikash", "vikas5a@gmail.com", 122.0));
+		
+		
+		// Iterate using the for each and entry  
+		Set<Entry<Integer, Employe>> enterySet = map.entrySet();
+		for (Entry<Integer, Employe> entry : enterySet) {
+			System.out.print(entry.getKey());
+			System.out.println(entry.getValue());
+		}
+		
+		
+	}
+
+}
