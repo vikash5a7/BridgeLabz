514c07f716d329a689aeea28b070841ac5891e48
Vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-01-06 15:53:46 +0530

    [vikash]Refactore. java8 Stream Api parallelStram

--- src/main/java/com/BridgeLabz/java8feature/StramApi/ClientsArray.java
+++ src/main/java/com/BridgeLabz/java8feature/StramApi/ClientsArray.java
@@ -6,26 +6,24 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class ClientsArray {
-public static void main(String[] args) {
-	String arr[] = new String[] {"vikash","ram", "ravi"};
-	 Stream<String> stream= Arrays.stream(arr);
-	 stream.forEach(System.out::println);
-	 
-	 
-	 System.out.println("-------------------------------------------------");
-	 Stream<String> of = Stream.of(arr);
-	 of.forEach(System.out::println);
-	 
-	 
-	 List<String> list= new ArrayList<String>();
-	 list.add("vikash");
-	 list.add("sanu");
-	 list.add("ram");
-	 list.add("radhe");
-	 
-	 System.out.println("-------------------------------------------------");
-	 Stream<String> streamvalue = list.stream(); 
-	 streamvalue.forEach(System.out::println);
-	
-}
+	public static void main(String[] args) {
+		String arr[] = new String[] { "vikash", "ram", "ravi" };
+		Stream<String> stream = Arrays.stream(arr);
+		stream.forEach(System.out::println);
+
+		System.out.println("-------------------------------------------------");
+		Stream<String> of = Stream.of(arr);
+		of.forEach(System.out::println);
+
+		List<String> list = new ArrayList<String>();
+		list.add("vikash");
+		list.add("sanu");
+		list.add("ram");
+		list.add("radhe");
+
+		System.out.println("-------------------------------------------------");
+		Stream<String> streamvalue = list.stream();
+		streamvalue.forEach(System.out::println);
+
+	}
 }
