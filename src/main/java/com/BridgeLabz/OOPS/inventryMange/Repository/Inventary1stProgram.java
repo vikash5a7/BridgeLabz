92250a091b7280fbd7d13a39e6c7fac0e090aae5
Vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-01-08 18:45:43 +0530

    [vikash]Refactore. Inventary Managaments

--- src/main/java/com/BridgeLabz/OOPS/inventryMange/Repository/Inventary1stProgram.java
+++ src/main/java/com/BridgeLabz/OOPS/inventryMange/Repository/Inventary1stProgram.java
@@ -14,8 +14,8 @@ public class Inventary1stProgram {
 			Object obj = jsonParser.parse(file);
 			JSONObject jsonOb = (JSONObject) obj;
 			JSONObject inventaryDetails = (JSONObject) jsonOb.get("Inventary");
-	
-			// getting the objects of the Wheat 
+
+			// getting the objects of the Wheat
 			System.out.println("------Information about wheat from inventary JSSON-----");
 			JSONObject weatDetails = (JSONObject) inventaryDetails.get("wheat");
 			System.out.println(weatDetails.toJSONString());
@@ -25,9 +25,32 @@ public class Inventary1stProgram {
 			System.out.println("Price of Wheat: " + pricePerKg);
 			Double weight = (Double) weatDetails.get("Weight");
 			System.out.println("Total Weight of Wheat " + weight);
-			System.out.println("Total cost of Weat-> " + pricePerKg*weight );
-			
-			
+			System.out.println("Total cost of Weat-> " + pricePerKg * weight);
+
+			// getting the objects of the pulse
+			System.out.println("\n------Information about Pulse from inventary JSSON-----");
+			JSONObject pulseDetails1 = (JSONObject) inventaryDetails.get("pulses");
+			System.out.println(pulseDetails1.toJSONString());
+			String pulseName = (String) pulseDetails1.get("Name");
+			System.out.println("Wheat Name: " + pulseName);
+			Double pulsePrice = (Double) pulseDetails1.get("PricePerKg");
+			System.out.println("Price of Wheat: " + pulsePrice);
+			Double pulseWeight = (Double) pulseDetails1.get("Weight");
+			System.out.println("Total Weight of Wheat " + pulseWeight);
+			System.out.println("Total cost of Weat-> " + pulsePrice * pulseWeight);
+
+			// getting the objects of the rice
+			System.out.println("\n------Information about Pulse from inventary JSSON-----");
+			JSONObject riceDetails = (JSONObject) inventaryDetails.get("Rice");
+			System.out.println(riceDetails.toJSONString());
+			String riceName = (String) riceDetails.get("Name");
+			System.out.println("Wheat Name: " + riceName);
+			Double ricePrice = (Double) riceDetails.get("PricePerKg");
+			System.out.println("Price of Wheat: " + ricePrice);
+			Double riceweight = (Double) riceDetails.get("Weight");
+			System.out.println("Total Weight of Wheat " + riceweight);
+			System.out.println("Total cost of Weat-> " + ricePrice * riceweight);
+
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println();
