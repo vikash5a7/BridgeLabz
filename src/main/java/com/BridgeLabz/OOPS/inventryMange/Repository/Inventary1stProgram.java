4f0b2fb0f34716335bbd388680177065376022a5
Vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-01-08 18:36:40 +0530

    [vikash]Add. Inventary Managments

--- src/main/java/com/BridgeLabz/OOPS/inventryMange/Repository/Inventary1stProgram.java
+++ src/main/java/com/BridgeLabz/OOPS/inventryMange/Repository/Inventary1stProgram.java
@@ -1,50 +1,38 @@
 package com.BridgeLabz.OOPS.inventryMange.Repository;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
-import java.util.function.BiConsumer;
 
-import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class Inventary1stProgram {
-	private static void parseInventory(JSONObject inv) {
-		// Get employee website name
-		// Get employee object within list
-		JSONObject employeeObject = (JSONObject) inv.get("Inventory");
 
-		String name = (String) employeeObject.get("Name");
-		System.out.println("Name  : " + name);
-
-		String Weight = String.valueOf(employeeObject.get("Weight"));
-		// int weight = Integer.parseInt(lastName);
-		System.out.println("Weight: " + Weight);
-
-		String Price = String.valueOf(employeeObject.get("PricePerKg"));
-		// int Price = Integer.parseInt(firstName);
-		System.out.println("Price : " + Price);
-		System.out.println();
-	}
-
-	@SuppressWarnings("unchecked")
 	public static void readFile() {
+		System.out.println("==========================================");
+		JSONParser jsonParser = new JSONParser();
+		try (FileReader file = new FileReader("Inventary.json")) {
+			Object obj = jsonParser.parse(file);
+			JSONObject jsonOb = (JSONObject) obj;
+			JSONObject inventaryDetails = (JSONObject) jsonOb.get("Inventary");
 	
-		try (FileReader reader = new FileReader(
-				"/home/user/Desktop/bridgeLab/src/main/java/com/BridgeLabz/OOPS/inventryMange/service/inventary.json")) {
+			// getting the objects of the Wheat 
+			System.out.println("------Information about wheat from inventary JSSON-----");
+			JSONObject weatDetails = (JSONObject) inventaryDetails.get("wheat");
+			System.out.println(weatDetails.toJSONString());
+			String weatsName = (String) weatDetails.get("Name");
+			System.out.println("Wheat Name: " + weatsName);
+			Double pricePerKg = (Double) weatDetails.get("PricePerKg");
+			System.out.println("Price of Wheat: " + pricePerKg);
+			Double weight = (Double) weatDetails.get("Weight");
+			System.out.println("Total Weight of Wheat " + weight);
+			System.out.println("Total cost of Weat-> " + pricePerKg*weight );
 			
-			JSONParser parser = new JSONParser();
-			Object perserOb = parser.parse(reader);
 			
-
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		} catch (Exception e) {
 			e.printStackTrace();
+			System.out.println();
 		}
+
 	}
 
 }
