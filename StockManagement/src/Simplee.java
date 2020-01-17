8a2e167bf6573e9cccd04dc743cf5debdcea6e7d
vikash5a7 <vikashkumargupta5a7@gmail.com>
2020-01-17 13:46:58 +0530

    [vikash]Add. STOCK MANAGMENTS

--- StockManagement/src/Simplee.java
+++
@@ -1,9 +0,0 @@
-
-public class Simplee {
-public static void main(String[] args) {
-Double am = 12.0; 
-Double pr = 1234.0;
-
-	System.out.println(Math.round(pr/am));
-}
-}
--- StockManagement/src/com/bridgelabz/stockaccountmanagement/repository/package-info.java
+++
@@ -1,1 +0,0 @@
-package com.bridgelabz.stockaccountmanagement.repository;
